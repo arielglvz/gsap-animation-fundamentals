85bb0a4a303c97c52b155249260c877f2823d6fb
arielglvz <arielglvz@gmail.com>
2026-05-06 18:25:11 +0800

    fix: hero video position

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
       .to(".left-leaf", { y: -200 }, 0)
 
     const startValue = isMobile ? "top 50%" : "center 60%"
-    const endValue = isMobile ? "130% top" : "bottom top"
+    const endValue = isMobile ? "120% top" : "bottom top"
 
     // Video animation timeline
     // Create the timeline wiith a default duration
